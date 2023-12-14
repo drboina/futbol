c1084bc8e43413a0f828bd61798beae6ad0518c7
boinas <leonardo.baez@sonora.gob.mx>
2023-12-14 01:37:33 -0700

    stats table

--- ligamx.py
+++ ligamx.py
@@ -8,5 +8,14 @@ import re
 tabla = pd.read_html('https://www.espn.com.mx/futbol/posiciones/_/liga/mex.1',encoding='UTF-8')
 tabla=tabla[1]
 equipo=['América','Monterrey','Tigres UANL','Pumas UNAM','Guadalajara','Puebla','Atlético de San Luis','León','Santos','Mazatlán','Pachuca','Toluca','Tijuana','Querétaro','FC Juarez','Cruz Azul','Atlas','Necaxa']
+
 tabla.index=equipo
-print(tabla)
+GF_promedio=tabla.GF/tabla.J
+GC_promedio=tabla.GC/tabla.J
+goles_promedio_ligamx=tabla.GF.mean()
+ataque=GF_promedio/goles_promedio_ligamx
+defensa=GC_promedio/goles_promedio_ligamx
+
+df=pd.DataFrame({'Juegos':tabla.J,'Ganados':tabla.G, 'Perdidos':tabla.P, 'Empates':tabla.E,'GF promedio':GF_promedio, 'GC promedio':GC_promedio,'Ataque':ataque,'Defensa':defensa})
+
+print(df)
