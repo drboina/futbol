948146186730ad65bbc055b0650a4a0256eaf939
boinas <leonardo.baez@sonora.gob.mx>
2023-12-13 22:27:32 -0700

    tabla gral

--- ligamx.py
+++ ligamx.py
@@ -3,9 +3,10 @@ import numpy as np
 from scipy.stats import poisson 
 from matplotlib import pyplot as plt
 import seaborn as sns
+import re
 
 tabla = pd.read_html('https://www.espn.com.mx/futbol/posiciones/_/liga/mex.1',encoding='UTF-8')
-equipo=tabla[0]
 tabla=tabla[1]
-print(equipo)
+equipo=['América','Monterrey','Tigres UANL','Pumas UNAM','Guadalajara','Puebla','Atlético de San Luis','León','Santos','Mazatlán','Pachuca','Toluca','Tijuana','Querétaro','FC Juarez','Cruz Azul','Atlas','Necaxa']
+tabla.index=equipo
 print(tabla)
