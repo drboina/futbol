81bc033fb9728d8bbcb8b9a39a030bc950a896eb
boinas <leonardo.baez@sonora.gob.mx>
2024-02-19 20:01:34 -0700

    tabla y Gx

--- ligamx.py
+++ ligamx.py
@@ -6,27 +6,54 @@ import seaborn as sns
 import re
 import lxml
 
-data = pd.read_html('https://www.espn.com.mx/futbol/posiciones/_/liga/mex.1',encoding='UTF-8')
+#Limpieza y tratamiento de datos
+equipos = pd.read_html("https://www.goal.com/es-mx/noticias/tabla-general-liga-mx/gotsl4uias0z1nyvu8pktnv1b",encoding='UTF-8')
+equipo = equipos[0]
+team = equipo[1]
+club = team[1:]
+
+tabla_gral = pd.read_html("https://espndeportes.espn.com/futbol/posiciones/_/liga/mex.1/liga-mx", encoding='UTF-8')
+tabla = tabla_gral[1]
+club.index = tabla.index
+tabla['Club'] = club
+
 calendario=pd.read_csv('CALENDARIO.csv')
+index = calendario['FECHA'][60:81]
+local = str(calendario['LOCAL'][60:70])
+visita = str(calendario['VISITA'][60:70])
 
-
-tabla=data[1]
-equipo=data[0]
-equipo=['América','Atlas','Atlético de San Luis','Tijuana','Cruz Azul','Guadalajara','FC Juarez','León','Mazatlán FC','Monterrey','Necaxa','Pachuca','Puebla','Pumas UNAM','Querétaro','Santos','Tigres UANL','Toluca']
-tabla.index=equipo
+#Cálculos estadisticos
 GF_promedio=tabla.GF/tabla.J
 GC_promedio=tabla.GC/tabla.J
-goles_promedio_ligamx=tabla.GF.mean()
+goles_promedio_ligamx=tabla.GF.sum()/tabla.J.sum()
 ataque=GF_promedio/goles_promedio_ligamx
 defensa=GC_promedio/goles_promedio_ligamx
+tabla['Ataque'] = ataque
+tabla['GF_promedio'] = GF_promedio
+tabla['Defensa'] = defensa
+tabla['GC_promedio'] = GC_promedio
 
-df=pd.DataFrame({'Juegos':tabla.J,'Ganados':tabla.G, 'Perdidos':tabla.P, 'Empates':tabla.E,'GF promedio':GF_promedio, 'GC promedio':GC_promedio,'Ataque':ataque,'Defensa':defensa})
-
-print(df)
-local=calendario.LOCAL[0:153]
-visita=calendario.VISITA[0:153]
-
-i=range(0,153)
-print(local[i])
-print(visita[i])
-
+#Display de resultados
+print(f'\n goles promedio de ligamx: {goles_promedio_ligamx} por partido\n')
+print(tabla)
+print('\n')
+print(f'{tabla.loc[15]["Club"]}: {tabla.loc[15]["Ataque"]*tabla.loc[3]["Defensa"]*goles_promedio_ligamx}')
+print('vs')
+print(f'{tabla.loc[3]["Club"]}: {tabla.loc[3]["Ataque"]*tabla.loc[15]["Defensa"]*goles_promedio_ligamx}')
+print('\n')
+print(f'{tabla.loc[7]["Club"]}: {tabla.loc[7]["Ataque"]*tabla.loc[6]["Defensa"]*goles_promedio_ligamx}')
+print('vs')
+print(f'{tabla.loc[6]["Club"]}: {tabla.loc[6]["Ataque"]*tabla.loc[7]["Defensa"]*goles_promedio_ligamx}')
+print('\n')
+print(f'{tabla.loc[8]["Club"]}: {tabla.loc[8]["Ataque"]*tabla.loc[16]["Defensa"]*goles_promedio_ligamx}')
+print('vs')
+print(f'{tabla.loc[16]["Club"]}: {tabla.loc[16]["Ataque"]*tabla.loc[8]["Defensa"]*goles_promedio_ligamx}')
+print('\n')
+print(f'{tabla.loc[4]["Club"]}: {tabla.loc[4]["Ataque"]*tabla.loc[12]["Defensa"]*goles_promedio_ligamx}')
+print('vs')
+print(f'{tabla.loc[12]["Club"]}: {tabla.loc[12]["Ataque"]*tabla.loc[4]["Defensa"]*goles_promedio_ligamx}')
+print('\n')
+print(f'{tabla.loc[10]["Club"]}: {tabla.loc[10]["Ataque"]*tabla.loc[0]["Defensa"]*goles_promedio_ligamx}')
+print('vs')
+print(f'{tabla.loc[0]["Club"]}: {tabla.loc[0]["Ataque"]*tabla.loc[10]["Defensa"]*goles_promedio_ligamx}')
+print('\n')
