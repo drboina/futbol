0d5411e06c5f06043ab167e3e4ba8baac0081789
boinas <leonardo.baez@sonora.gob.mx>
2024-08-20 02:33:44 -0700

    20 agosto

--- ligamx.py
+++ ligamx.py
@@ -15,18 +15,25 @@ def clean_data():
 tabla = clean_data()
 print(f'{tabla}\n')
 
-def promedio():
+def promedio_goles():
     promedio = tabla.GF.sum()/tabla.J.sum()
-    print(f'No. goles promedio en liga: {promedio}\n')
     return promedio
 
-goles_prom_liga = promedio()
+goles_prom_liga = promedio_goles()
 
 def multiplicar_elementos(lista1, lista2):
     return [lista1[i] * lista2[i] * goles_prom_liga for i in range(len(lista1))]
 
-local = [16,14,1,0,4,10,13,6,17]
-visita = [5,15,12,9,2,3,7,8,11]
+def juegos(jornada):
+    game = pd.read_html("https://mexico.as.com/resultados/futbol/mexico_apertura/calendario/", encoding='UTF-8')
+    return game[jornada]
+
+jornada = 4
+partidos = juegos(jornada)
+print("\n",partidos.iloc[:])
+
+local = [17,16,7,10,5,13,3,8,4]
+visita = [1,11,0,14,2,15,6,12,9]
 
 ataque_local = tabla.Ataque[local].to_list()
 defensa_local = tabla.Defensa[local].to_list()
@@ -36,6 +43,8 @@ defensa_visita = tabla.Defensa[visita].to_list()
 xG_local = multiplicar_elementos(ataque_local, defensa_visita)
 xG_visita = multiplicar_elementos(ataque_visita, defensa_local)
 
-for i in range(len(xG_local)):
-    print(f'{tabla.Club[local[i]]}:{xG_local[i]} \nvs\n{tabla.Club[visita[i]]}:{xG_visita[i]}\n')
+pronostico = pd.DataFrame({"Local":tabla.Club[local].to_list(),"xG_local":xG_local,"Visita":tabla.Club[visita].to_list(),"xG_visita":xG_visita})
+print("\n",pronostico)
+pronostico.to_csv(f"pronostico_jormada_{jornada}.csv")
+        
 
