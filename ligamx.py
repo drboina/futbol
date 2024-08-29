8d99bf62218713632447dbd27582da382c5c96ff
boinas <leonardo.baez@sonora.gob.mx>
2024-08-28 22:38:28 -0700

    28octubre

--- ligamx.py
+++ ligamx.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def clean_data():
     tabla = pd.read_html("https://www.goal.com/es-mx/noticias/tabla-general-liga-mx/gotsl4uias0z1nyvu8pktnv1b",encoding='UTF-8')
@@ -28,12 +29,13 @@ def juegos(jornada):
     game = pd.read_html("https://mexico.as.com/resultados/futbol/mexico_apertura/calendario/", encoding='UTF-8')
     return game[jornada]
 
-jornada = 4
+jornada=5
 partidos = juegos(jornada)
-print("\n",partidos.iloc[:])
+print(partidos)
 
-local = [17,16,7,10,5,13,3,8,4]
-visita = [1,11,0,14,2,15,6,12,9]
+
+local=[7,13,5,14,8,1,0,6,15]
+visita = [4,10,12,17,16,2,11,3,9]
 
 ataque_local = tabla.Ataque[local].to_list()
 defensa_local = tabla.Defensa[local].to_list()
@@ -43,8 +45,11 @@ defensa_visita = tabla.Defensa[visita].to_list()
 xG_local = multiplicar_elementos(ataque_local, defensa_visita)
 xG_visita = multiplicar_elementos(ataque_visita, defensa_local)
 
-pronostico = pd.DataFrame({"Local":tabla.Club[local].to_list(),"xG_local":xG_local,"Visita":tabla.Club[visita].to_list(),"xG_visita":xG_visita})
-print("\n",pronostico)
+pronostico = pd.DataFrame({"Local":tabla.Club[local].to_list(),"xG_local":xG_local,"xG_visita":xG_visita,"Visita":tabla.Club[visita].to_list()})
+diferencia = round(pronostico.xG_local - pronostico.xG_visita)
+pronostico["diferencia"] = diferencia  
 pronostico.to_csv(f"pronostico_jormada_{jornada}.csv")
+print("\n",pronostico)
+
         
 
