76ec6aa4d77db0670b76f370ca3def93e7793dc7
dr. boina <152813972+drboina@users.noreply.github.com>
2024-09-14 18:02:00 -0700

    14 sep

--- ligamx.py
+++ ligamx.py
@@ -1,5 +1,6 @@
 import pandas as pd
-import numpy as np
+
+jornada = int(input("No. de la jornada: "))
 
 def clean_data():
     tabla = pd.read_html("https://www.goal.com/es-mx/noticias/tabla-general-liga-mx/gotsl4uias0z1nyvu8pktnv1b",encoding='UTF-8')
@@ -14,7 +15,13 @@ def clean_data():
     return tabla
 
 tabla = clean_data()
-print(f'{tabla}\n')
+
+def juegos(jornada):
+    game = pd.read_html("https://mexico.as.com/resultados/futbol/mexico_apertura/calendario/", encoding='UTF-8')
+    return game[jornada]
+
+partidos = juegos(jornada)
+resultados = juegos(jornada-1)
 
 def promedio_goles():
     promedio = tabla.GF.sum()/tabla.J.sum()
@@ -29,32 +36,31 @@ def juegos(jornada):
     game = pd.read_html("https://mexico.as.com/resultados/futbol/mexico_apertura/calendario/", encoding='UTF-8')
     return game[jornada]
 
-jornada=6
 partidos = juegos(jornada)
 resultados = juegos(jornada-1)
-print(f"Resultados Jornada {jornada-1}")
-print(resultados)
-print("\n")
-print(f"Pronostico Jornada {jornada}")
-print(partidos)
 
-local = [9,7, 16, 1, 10, 2, 11,15, 14]
-visita = [17, 13, 12, 6, 8, 3, 5, 0,4]
+local = [4,7,11,14,17,10,6,12,5]
+visita = [13,11,3,2,0,8,15,9,16]
 
-ataque_local = tabla.Ataque[local].to_list()
-defensa_local = tabla.Defensa[local].to_list()
-ataque_visita = tabla.Ataque[visita].to_list()
-defensa_visita = tabla.Defensa[visita].to_list()
+def xG(local,visita):
+    ataque_local = tabla.Ataque[local].to_list()
+    defensa_local = tabla.Defensa[local].to_list()
+    ataque_visita = tabla.Ataque[visita].to_list()
+    defensa_visita = tabla.Defensa[visita].to_list()
+    return [ataque_local, ataque_visita, defensa_local, defensa_visita]
+
+[ataque_local, ataque_visita, defensa_local, defensa_visita] = xG(local,visita)
 
 xG_local = multiplicar_elementos(ataque_local, defensa_visita)
 xG_visita = multiplicar_elementos(ataque_visita, defensa_local)
 
 pronostico = pd.DataFrame({"Local":tabla.Club[local].to_list(),"xG_local":xG_local,"xG_visita":xG_visita,"Visita":tabla.Club[visita].to_list()})
-diferencia = round(pronostico.xG_local - pronostico.xG_visita)
-pronostico["diferencia"] = diferencia  
 pronostico.to_csv(f"pronostico_jormada_{jornada}.csv")
 resultados.to_csv(f"resultados_jornada_{jornada-1}.csv")
+print(f'\n{tabla}\n')
+print("\n",resultados)
 print("\n",pronostico)
+print("\n",partidos)
 
         
 
